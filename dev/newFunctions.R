fbfd45de4c9bfc4b1d8c7319acff13281710b7ba
jberry47 <jberry47@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-12 19:43:59 +0000

    Updated with Beauchemin simulation. All the helper functions are included.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motilitylab@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- dev/newFunctions.R
+++ dev/newFunctions.R
@@ -90,44 +90,209 @@ gbm.tracks <- function(nsteps=100,ncells,randlength=F,lag=0,drift=0){
   }
 }
 
+#' Helper function of beauWalker()
+#' This function samples uniformly from unit sphere
+sphere_sampler <- function(){
+  x <- rnorm(3)
+  z <- sqrt(sum(x^2))
+  out <- c(x[1]/z,x[2]/z,x[3]/z)
+  return(out)
+  
+}
 
-#for(i in i to nCells){
-#  walkers[i] <- walker(v_free, t_free,t_pause,delta_t, runif(1), runif(1), runif(1))
-#  pos[i] <- something with walkers[i]
-#}
+#' Helper function of beauWalker()
+#' This function samples uniformly from unit circle
+circle_sampler <- function(){
+  x <- rnorm(2)
+  z <- sqrt(sum(x^2))
+  out <- c(x[1]/z,x[2]/z)
+  return(out)
+}
 
-#walker <- function(v_free, t_free,t_pause,delta_t, x, y, z){
-#  t_free_rest = runif(1) * (t_free + t_pause) - t_pause
-#  pos <- c(x,y,z)
-#  direction <- c(0,0,0)
-#  pick_new_direction();
-#}
-#
-#pick_new_direction <- function(){
-#  u <- runif(1)
-#  v <- runif(1)
-#  theta <- 2*pi*u
-#  phi <- cos(2*v-1)
-#  direction[1] <- (cos(theta)+sin(phi))
-#  direction[2] <- (sin(theta)+sin(phi))
-#  direction[3] <- cos(phi)
-#  direction <- direction*v_free
-#}
+#' Helper function of beauWalker()
+#' This function samples from the trianglular distribution
+triangular_sampler <- function(){
+  x <- seq(from=-1,to=1,by=0.0001)
+  y <- (x+1)/2
+  z <- sample(x,1,prob=y/10000)
+  return(z)
+}
+
+#' Helper function of beauWalker() and is not directly called by user
+#' @param pbias Strength of movement in a given direction
+#' @param taxis_mode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis
+#' @param tfree Time interval for how long the cell is able to move between steps
+#' @param vfree Speed of cell
+pick_new_direction <- function(pbias,taxis_mode,tfree,vfree){
+  d <- sphere_sampler()
+  
+  if(taxis_mode == 2){
+    if(runif(1) < pbias){
+      # topotaxis
+      d[3] <- triangular_sampler()
+      circ <- circle_sampler()
+      circle_scale <- sqrt(1-d[3]^2)
+      d[1] <- circ[1]*circle_scale
+      d[2] <- circ[2]*circle_scale
+    }
+    d <- c(d*vfree,0)
+  }
+  if(taxis_mode == 3){
+    if(pbias > 0){
+      # klinotaxis
+      tfreerest = tfreerest + sum(tfree * pbias * d);
+    }
+    d <- c(d*vfree,tfreerest)  
+  }
+  else{
+    # orthotaxis
+    d <- c(d*vfree*(1+pbias*d),0)
+  }
+  return(d)
+}
 
 
 
+#' Simulates a single cell track based on model written by J. Textor, M. Sinn, and R. Boer called the Beauchemin model. 
+#' 
+#' @param nsteps Specifies the number of steps for the model to take
+#' @param deltat Change in time between each timepoint
+#' @param ppersist Indicates how probable a change in direction is. ppersist = 1 indicates there is never a change in direction between steps and ppersist = 0 indicates there is always a change in direction between steps
+#' @param pbias Strength of movement in a given direction. Equates to the radius of sphere where the new direction is sampled. A pbias = 1 indicates sampling from unit sphere. 
+#' @param taxis_mode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis
+#' @param tfree Time interval for how long the cell is able to move between steps
+#' @param vfree Speed of cell 
+#' @param tpause Time for how long the cell takes to adjust movement to new direction
+#' 
+#' @details The default parameters are those found by mean-square displacement plots created by Beauchemin et al.
+#' @return A matrix of size Nx5 where N is the number of timepoints allowed based number of \code{nsteps} allowed. The 
+#' first column indicates the time change from the point previous to the current point. The second column is an 
+#' index of timepoints (there are multiple timepoints allowed in one step). The remaining three columns are the 
+#' three dimensional coordinates of the cell at that timepoint. 
+#' @examples 
+#' ## Create track with model parameters and return matrix of positions
+#' out <- beauWalker(nsteps=20,ppersist = 0.3,taxis_mode = 1)
+#' ## Plot X-Y projection
+#' plot(PosY~PosX,data=out,type='b')
 
-#' This will be the start of trying to use fractal dimensions to determine which model 
-#' the set of tracks fits the best. 
-#' TODO: will be worthwile to try to create MLE functions to assign a probability 
-#' for any given model. 
-# fdsims <- NULL
-# for(i in 1:10000){
-#  BMtrack <- BM()
-#  fdout <- fd.estim.variogram(matrix(BMtrack))
-#  fdsims <- c(fdsims,fdout$fd)
-#}
-#sdfdsims <- sd(fdsims)
-#UL <- 1.5+2*sdfdsims
-#LL <- 1.5-2*sdfdsims
-#cbind(LL,UL)
+
+beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,tfree=2,vfree=18.8,tpause=0.5){  
+  
+  pausedStep <- function(deltat){
+    if(-tfreerest+deltat < tpause ){
+      tfreerest = tfreerest-deltat
+      newpos <- c(i*deltat*60, i, pos[length(pos[,1]),3:5])
+      return(c(tfreerest,1,newpos))
+    }
+    else{
+      deltatrest= deltat - tpause - tfreerest
+      tfreerest = tfree
+      paused = FALSE
+      
+      if (runif(1) < 1-ppersist){
+        d <- pick_new_direction(pbias=pbias,taxis_mode=taxis_mode,tfree=tfree,vfree=vfree)
+        if(d[4]==0){
+          d <- d[1:3]
+        }
+        else{
+          tfreerest <- d[4]
+          d <- d[1:3]
+        }
+      }
+      return(c(deltatrest,2,d,tfreerest))
+    }
+  }
+  
+  
+  movingStep <- function(deltat){
+    if( deltat < tfreerest ){
+      newpos <- c(i*deltat*60,i,pos[length(pos[,1]),3:5] + d*deltat)
+      tfreerest = tfreerest-deltat
+      return(c(tfreerest,1,newpos))
+    }
+    else{
+      deltatrest = deltat - tfreerest
+      newpos <- c(i*deltat*60,i,pos[length(pos[,1]),3:5] + d*tfreerest)
+      paused = TRUE
+      tfreerest = 0.0 
+      return(c(deltatrest,2,newpos))
+    }
+  }
+  
+  pos <- matrix(c(deltat,0,0,0,0),1,5)
+  tfreerest = tfree
+  paused = tfreerest < 0.0
+  deltatresti <- deltatrest <- deltat - tpause - tfreerest
+  d <- pick_new_direction(pbias=pbias,taxis_mode=taxis_mode,tfree=tfree,vfree=vfree)
+  if(d[4]==0){
+    d <- d[1:3]
+  }
+  else{
+    tfreerest <- d[4]
+    d <- d[1:3]
+  }
+  
+  i <- 1
+  j <- 0
+  repeat{
+    if(paused==TRUE){
+      catch <- pausedStep(deltat)
+      if(catch[2]==1){
+        pos <- rbind(pos,catch[3:7])
+        i <- i+1
+        tfreerest <- catch[1]
+      }
+      else{
+        deltatrest <- catch[1]
+        d <- catch[3:5]
+        tfreerest<-catch[6]
+        paused = FALSE
+      }
+      if( deltatrest > 0 ){
+        catch <- movingStep(deltatrest)
+        if(catch[2]==1){
+          tfreerest <- catch[1]
+          pos <- rbind(pos,catch[3:7])
+          i <- i+1
+        }
+        else{
+          deltatrest <- catch[1]
+          pos <- rbind(pos,catch[3:7])
+          i <- i+1
+        }
+      }
+    }
+    else{
+      catch <- movingStep(deltat)
+      if(catch[2]==1){
+        tfreerest <- catch[1]
+        pos <- rbind(pos,catch[3:7])
+        i <- i+1
+      }
+      else{
+        deltatrest <- catch[1]
+        pos <- rbind(pos,catch[3:7])
+        i <- i+1
+        paused <- TRUE
+      }
+      if( deltatrest > 0 ){
+        catch <- pausedStep(deltatrest)
+        if(catch[2]==1){
+          tfreerest <- catch[1]
+          pos <- rbind(pos,catch[3:7])
+          i <- i+1
+        }
+        else{
+          deltatrest <- catch[1]
+        }
+      }
+    }
+    j <- j+1
+    #or should we use i here instead? it's more intuitive and predictable if we use i... 
+    if(j == nsteps){
+      break
+    }
+  }
+  colnames(pos) <- c("Delta_t","TimePoint","PosX","PosY","PosZ")
+  return(pos)
+}
