b8e045484347bab72417b2dd153e368b9c3edb33
jberry47 <jberry47@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-14 19:07:06 +0000

    Instead of having the beauWalker() helper functions outside of the main function, I added them inside. Saves some trouble with documentation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motilitylab@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- dev/newFunctions.R
+++ dev/newFunctions.R
@@ -90,68 +90,6 @@ gbm.tracks <- function(nsteps=100,ncells,randlength=F,lag=0,drift=0){
   }
 }
 
-#' Helper function of beauWalker()
-#' This function samples uniformly from unit sphere
-sphere_sampler <- function(){
-  x <- rnorm(3)
-  z <- sqrt(sum(x^2))
-  out <- c(x[1]/z,x[2]/z,x[3]/z)
-  return(out)
-  
-}
-
-#' Helper function of beauWalker()
-#' This function samples uniformly from unit circle
-circle_sampler <- function(){
-  x <- rnorm(2)
-  z <- sqrt(sum(x^2))
-  out <- c(x[1]/z,x[2]/z)
-  return(out)
-}
-
-#' Helper function of beauWalker()
-#' This function samples from the trianglular distribution
-triangular_sampler <- function(){
-  x <- seq(from=-1,to=1,by=0.0001)
-  y <- (x+1)/2
-  z <- sample(x,1,prob=y/10000)
-  return(z)
-}
-
-#' Helper function of beauWalker() and is not directly called by user
-#' @param pbias Strength of movement in a given direction
-#' @param taxis_mode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis
-#' @param tfree Time interval for how long the cell is able to move between steps
-#' @param vfree Speed of cell
-pick_new_direction <- function(pbias,taxis_mode,tfree,vfree){
-  d <- sphere_sampler()
-  
-  if(taxis_mode == 2){
-    if(runif(1) < pbias){
-      # topotaxis
-      d[3] <- triangular_sampler()
-      circ <- circle_sampler()
-      circle_scale <- sqrt(1-d[3]^2)
-      d[1] <- circ[1]*circle_scale
-      d[2] <- circ[2]*circle_scale
-    }
-    d <- c(d*vfree,0)
-  }
-  if(taxis_mode == 3){
-    if(pbias > 0){
-      # klinotaxis
-      tfreerest = tfreerest + sum(tfree * pbias * d);
-    }
-    d <- c(d*vfree,tfreerest)  
-  }
-  else{
-    # orthotaxis
-    d <- c(d*vfree*(1+pbias*d),0)
-  }
-  return(d)
-}
-
-
 
 #' Simulates a single cell track based on model written by J. Textor, M. Sinn, and R. Boer called the Beauchemin model. 
 #' 
@@ -174,10 +112,91 @@ pick_new_direction <- function(pbias,taxis_mode,tfree,vfree){
 #' out <- beauWalker(nsteps=20,ppersist = 0.3,taxis_mode = 1)
 #' ## Plot X-Y projection
 #' plot(PosY~PosX,data=out,type='b')
-
-
 beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,tfree=2,vfree=18.8,tpause=0.5){  
+  #' Parameter checks
+  if(ppersist < 0 || ppersist > 1){
+    stop("ppersist must be a value between 0 and 1")
+  }
+  if(sum(taxis_mode == c(1,2,3))==0){
+    stop("taxis_mode can either be 1, 2, or 3. See documentation for details on which model you would like to use.")
+  }
+  if(pbias <= 0){
+    stop("pbias must be a value greater than 0")
+  }
+  if(nsteps <= 0){
+    stop("nsteps must be a value larger than 0")
+  }
+  if(deltat <= 0){
+    stop("deltat must be a value larger than 0")
+  }
+  if(tpause <= 0){
+    stop("tpause must be a value larger than 0")
+  }
+  if(tfree <= 0){
+    stop("tfree must be a value larger than 0")
+  }
+  if(vfree <= 0){
+    stop("vfree must be a value larger than 0")
+  }
   
+  #' Helper function of beauWalker()
+  #' This function samples uniformly from unit sphere
+  sphere_sampler <- function(){
+    x <- rnorm(3)
+    z <- sqrt(sum(x^2))
+    out <- c(x[1]/z,x[2]/z,x[3]/z)
+    return(out)
+    
+  }
+  
+  #' Helper function of beauWalker()
+  #' This function samples uniformly from unit circle
+  circle_sampler <- function(){
+    x <- rnorm(2)
+    z <- sqrt(sum(x^2))
+    out <- c(x[1]/z,x[2]/z)
+    return(out)
+  }
+  
+  #' Helper function of beauWalker()
+  #' This function samples from the trianglular distribution
+  triangular_sampler <- function(){
+    x <- seq(from=-1,to=1,by=0.0001)
+    y <- (x+1)/2
+    z <- sample(x,1,prob=y/10000)
+    return(z)
+  }
+  
+  #' Helper function of beauWalker() and is not directly called by user
+  pick_new_direction <- function(pbias,taxis_mode,tfree,vfree){
+    d <- sphere_sampler()
+    
+    if(taxis_mode == 2){
+      if(runif(1) < pbias){
+        # topotaxis
+        d[3] <- triangular_sampler()
+        circ <- circle_sampler()
+        circle_scale <- sqrt(1-d[3]^2)
+        d[1] <- circ[1]*circle_scale
+        d[2] <- circ[2]*circle_scale
+      }
+      d <- c(d*vfree,0)
+    }
+    if(taxis_mode == 3){
+      if(pbias > 0){
+        # klinotaxis
+        tfreerest = tfreerest + sum(tfree * pbias * d);
+      }
+      d <- c(d*vfree,tfreerest)  
+    }
+    else{
+      # orthotaxis
+      d <- c(d*vfree*(1+pbias*d),0)
+    }
+    return(d)
+  }
+  
+  #' Helper function of beauWalker() and is not directly called by user
   pausedStep <- function(deltat){
     if(-tfreerest+deltat < tpause ){
       tfreerest = tfreerest-deltat
@@ -203,7 +222,7 @@ beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,t
     }
   }
   
-  
+  #' Helper function of beauWalker() and is not directly called by user
   movingStep <- function(deltat){
     if( deltat < tfreerest ){
       newpos <- c(i*deltat*60,i,pos[length(pos[,1]),3:5] + d*deltat)
@@ -219,6 +238,7 @@ beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,t
     }
   }
   
+  #' Initializing parameters 
   pos <- matrix(c(deltat,0,0,0,0),1,5)
   tfreerest = tfree
   paused = tfreerest < 0.0
@@ -234,6 +254,9 @@ beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,t
   
   i <- 1
   j <- 0
+  
+  
+  #' This is the primary loop that creates the 3d track
   repeat{
     if(paused==TRUE){
       catch <- pausedStep(deltat)
