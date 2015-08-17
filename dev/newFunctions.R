6f8b0fa4086e2748e14fadd53aa20ce327d5c14d
jberry47 <jberry47@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-17 20:24:47 +0000

    renamed variables and functions to be more standardized. Although some variable names now look sort of funny. Removed the functions inside beauWalker() to be outside and private. Updated triangularSampler() to be deterministic.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motilitylab@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- dev/newFunctions.R
+++ dev/newFunctions.R
@@ -91,16 +91,118 @@ gbm.tracks <- function(nsteps=100,ncells,randlength=F,lag=0,drift=0){
 }
 
 
+#' Helper function of beauWalker() and is not directly called by user
+#' This function samples uniformly from unit sphere
+.sphereSampler <- function(){
+  x <- rnorm(3)
+  z <- sqrt(sum(x^2))
+  out <- c(x[1]/z,x[2]/z,x[3]/z)
+  return(out)
+  
+}
+
+#' Helper function of beauWalker() and is not directly called by user
+#' This function samples uniformly from unit circle
+.circleSampler <- function(){
+  x <- rnorm(2)
+  z <- sqrt(sum(x^2))
+  out <- c(x[1]/z,x[2]/z)
+  return(out)
+}
+
+#' Helper function of beauWalker() and is not directly called by user
+#' This function samples from the trianglular distribution
+.triangularSampler <- function(){
+  x <- runif(1)
+  y <- -1+sqrt(4*x)
+  return(y)
+}
+
+
+#' Helper function of beauWalker() and is not directly called by user
+#' returns a direction for the cell to travel based on model parameters specified in beauWalker()
+.pickNewDirection <- function(pBias,taxisMode,tFree,vFree,tFreeRest){
+  d <- .sphereSampler()
+  
+  if(taxisMode == 2){
+    if(runif(1) < pBias){
+      # topotaxis
+      d[3] <- .triangularSampler()
+      circ <- .circleSampler()
+      circleScale <- sqrt(1-d[3]^2)
+      d[1] <- circ[1]*circleScale
+      d[2] <- circ[2]*circleScale
+    }
+    d <- c(d*vFree,0)
+  }
+  else if(taxisMode == 3){
+    if(pBias > 0){
+      # klinotaxis
+      tFreeRest = tFreeRest + sum(tFree * pBias * d);
+    }
+    d <- c(d*vFree,tFreeRest)  
+  }
+  else{
+    # orthotaxis
+    d <- c(d*vFree*(1+pBias*d),0)
+  }
+  return(d)
+}
+
+
+#' Helper function of beauWalker() and is not directly called by user
+#' Returns the next step in the beauWalker() if the cell is paused
+.pausedStep <- function(d,deltaT,tFreeRest,tPause,pBias,taxisMode,tFree,vFree,pos,i,pPersist){
+  if(-tFreeRest+deltaT < tPause ){
+    tFreeRest = tFreeRest-deltaT
+    newpos <- c(i, pos)
+    return(c(tFreeRest,1,newpos))
+  }
+  else{
+    deltaTrest= deltaT - tPause - tFreeRest
+    tFreeRest = tFree
+    
+    if (runif(1) < 1-pPersist){
+      d <- .pickNewDirection(tFreeRest=tFreeRest,pBias=pBias,taxisMode=taxisMode,tFree=tFree,vFree=vFree)
+      if(d[4]==0){
+        d <- d[1:3]
+      }
+      else{
+        tFreeRest <- d[4]
+        d <- d[1:3]
+      }
+    }
+    return(c(deltaTrest,2,d,tFreeRest))
+  }
+}
+
+
+#' Helper function of beauWalker() and is not directly called by user
+#' Returns the next step in the beauWalker() if the cell is moving
+.movingStep <- function(deltaT,tFreeRest,d,pos,i){
+  if( deltaT < tFreeRest ){
+    newpos <- c(i,pos + d*deltaT)
+    tFreeRest = tFreeRest-deltaT
+    return(c(tFreeRest,1,newpos))
+  }
+  else{
+    deltaTrest = deltaT - tFreeRest
+    newpos <- c(i,pos + d*tFreeRest)
+    return(c(deltaTrest,2,newpos))
+  }
+}
+
+#' TODO: For some combinations of paramters the function gets stuck?? Not sure if it's my computer or not... 
 #' Simulates a single cell track based on model written by J. Textor, M. Sinn, and R. Boer called the Beauchemin model. 
 #' 
 #' @param nsteps Specifies the number of steps for the model to take
-#' @param deltat Change in time between each timepoint
-#' @param ppersist Indicates how probable a change in direction is. ppersist = 1 indicates there is never a change in direction between steps and ppersist = 0 indicates there is always a change in direction between steps
-#' @param pbias Strength of movement in a given direction. Equates to the radius of sphere where the new direction is sampled. A pbias = 1 indicates sampling from unit sphere. 
-#' @param taxis_mode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis
-#' @param tfree Time interval for how long the cell is able to move between steps
-#' @param vfree Speed of cell 
-#' @param tpause Time for how long the cell takes to adjust movement to new direction
+#' @param deltaT Change in time between each timepoint
+#' @param pPersist Indicates how probable a change in direction is. pPersist = 1 indicates there is never a change in direction between steps and pPersist = 0 indicates there is always a change in direction between steps
+#' @param pBias Strength of movement in a given direction. Equates to the radius of sphere where the new direction is sampled. A pBias = 1 indicates sampling from unit sphere. 
+#' @param taxisMode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis
+#' @param tFree Time interval for how long the cell is able to move between steps
+#' @param vFree Speed of cell 
+#' @param tPause Time for how long the cell takes to adjust movement to new direction
 #' 
 #' @details The default parameters are those found by mean-square displacement plots created by Beauchemin et al.
 #' @return A matrix of size Nx5 where N is the number of timepoints allowed based number of \code{nsteps} allowed. The 
@@ -109,213 +211,111 @@ gbm.tracks <- function(nsteps=100,ncells,randlength=F,lag=0,drift=0){
 #' three dimensional coordinates of the cell at that timepoint. 
 #' @examples 
 #' ## Create track with model parameters and return matrix of positions
-#' out <- beauWalker(nsteps=20,ppersist = 0.3,taxis_mode = 1)
+#' out <- beauWalker(nsteps=20,pPersist = 0.3,taxisMode = 1)
 #' ## Plot X-Y projection
 #' plot(PosY~PosX,data=out,type='b')
-beauWalker <- function(nsteps = 10,deltat=1,ppersist=0.5,pbias=.9,taxis_mode=1,tfree=2,vfree=18.8,tpause=0.5){  
+beauWalker <- function(nsteps = 25,deltaT=1,pPersist=0.5,pBias=0.5,taxisMode=1,tFree=2,vFree=18.8,tPause=0.5){  
   #' Parameter checks
-  if(ppersist < 0 || ppersist > 1){
-    stop("ppersist must be a value between 0 and 1")
+  if(pPersist < 0 || pPersist > 1){
+    stop("pPersist must be a value between 0 and 1")
   }
-  if(sum(taxis_mode == c(1,2,3))==0){
-    stop("taxis_mode can either be 1, 2, or 3. See documentation for details on which model you would like to use.")
+  if(sum(taxisMode == c(1,2,3))==0){
+    stop("taxisMode can either be 1, 2, or 3. See documentation for details on which model you would like to use.")
   }
-  if(pbias <= 0){
-    stop("pbias must be a value greater than 0")
+  if(pBias < 0){
+    stop("pBias must be a value greater than or equal to 0")
   }
   if(nsteps <= 0){
     stop("nsteps must be a value larger than 0")
   }
-  if(deltat <= 0){
-    stop("deltat must be a value larger than 0")
+  if(deltaT <= 0){
+    stop("deltaT must be a value larger than 0")
   }
-  if(tpause <= 0){
-    stop("tpause must be a value larger than 0")
+  if(tPause <= 0){
+    stop("tPause must be a value larger than 0")
   }
-  if(tfree <= 0){
-    stop("tfree must be a value larger than 0")
+  if(tFree <= 0){
+    stop("tFree must be a value larger than 0")
   }
-  if(vfree <= 0){
-    stop("vfree must be a value larger than 0")
-  }
-  
-  #' Helper function of beauWalker()
-  #' This function samples uniformly from unit sphere
-  sphere_sampler <- function(){
-    x <- rnorm(3)
-    z <- sqrt(sum(x^2))
-    out <- c(x[1]/z,x[2]/z,x[3]/z)
-    return(out)
-    
-  }
-  
-  #' Helper function of beauWalker()
-  #' This function samples uniformly from unit circle
-  circle_sampler <- function(){
-    x <- rnorm(2)
-    z <- sqrt(sum(x^2))
-    out <- c(x[1]/z,x[2]/z)
-    return(out)
-  }
-  
-  #' Helper function of beauWalker()
-  #' This function samples from the trianglular distribution
-  triangular_sampler <- function(){
-    x <- seq(from=-1,to=1,by=0.0001)
-    y <- (x+1)/2
-    z <- sample(x,1,prob=y/10000)
-    return(z)
-  }
-  
-  #' Helper function of beauWalker() and is not directly called by user
-  pick_new_direction <- function(pbias,taxis_mode,tfree,vfree){
-    d <- sphere_sampler()
-    
-    if(taxis_mode == 2){
-      if(runif(1) < pbias){
-        # topotaxis
-        d[3] <- triangular_sampler()
-        circ <- circle_sampler()
-        circle_scale <- sqrt(1-d[3]^2)
-        d[1] <- circ[1]*circle_scale
-        d[2] <- circ[2]*circle_scale
-      }
-      d <- c(d*vfree,0)
-    }
-    if(taxis_mode == 3){
-      if(pbias > 0){
-        # klinotaxis
-        tfreerest = tfreerest + sum(tfree * pbias * d);
-      }
-      d <- c(d*vfree,tfreerest)  
-    }
-    else{
-      # orthotaxis
-      d <- c(d*vfree*(1+pbias*d),0)
-    }
-    return(d)
-  }
-  
-  #' Helper function of beauWalker() and is not directly called by user
-  pausedStep <- function(deltat){
-    if(-tfreerest+deltat < tpause ){
-      tfreerest = tfreerest-deltat
-      newpos <- c(i*deltat*60, i, pos[length(pos[,1]),3:5])
-      return(c(tfreerest,1,newpos))
-    }
-    else{
-      deltatrest= deltat - tpause - tfreerest
-      tfreerest = tfree
-      paused = FALSE
-      
-      if (runif(1) < 1-ppersist){
-        d <- pick_new_direction(pbias=pbias,taxis_mode=taxis_mode,tfree=tfree,vfree=vfree)
-        if(d[4]==0){
-          d <- d[1:3]
-        }
-        else{
-          tfreerest <- d[4]
-          d <- d[1:3]
-        }
-      }
-      return(c(deltatrest,2,d,tfreerest))
-    }
-  }
-  
-  #' Helper function of beauWalker() and is not directly called by user
-  movingStep <- function(deltat){
-    if( deltat < tfreerest ){
-      newpos <- c(i*deltat*60,i,pos[length(pos[,1]),3:5] + d*deltat)
-      tfreerest = tfreerest-deltat
-      return(c(tfreerest,1,newpos))
-    }
-    else{
-      deltatrest = deltat - tfreerest
-      newpos <- c(i*deltat*60,i,pos[length(pos[,1]),3:5] + d*tfreerest)
-      paused = TRUE
-      tfreerest = 0.0 
-      return(c(deltatrest,2,newpos))
-    }
+  if(vFree <= 0){
+    stop("vFree must be a value larger than 0")
   }
   
   #' Initializing parameters 
-  pos <- matrix(c(deltat,0,0,0,0),1,5)
-  tfreerest = tfree
-  paused = tfreerest < 0.0
-  deltatresti <- deltatrest <- deltat - tpause - tfreerest
-  d <- pick_new_direction(pbias=pbias,taxis_mode=taxis_mode,tfree=tfree,vfree=vfree)
+  pos <- matrix(c(0,0,0,0),1,4)
+  tFreeRest = tFree
+  paused = tFreeRest < 0.0
+  deltaTrest <- deltaT - tPause - tFreeRest
+  d <- .pickNewDirection(tFreeRest=tFreeRest,pBias=pBias,taxisMode=taxisMode,tFree=tFree,vFree=vFree)
   if(d[4]==0){
     d <- d[1:3]
   }
   else{
-    tfreerest <- d[4]
+    tFreeRest <- d[4]
     d <- d[1:3]
   }
-  
   i <- 1
-  j <- 0
-  
   
   #' This is the primary loop that creates the 3d track
   repeat{
     if(paused==TRUE){
-      catch <- pausedStep(deltat)
+      catch <- .pausedStep(d=d,pPersist=pPersist,i=i,deltaT=deltaT,tFreeRest=tFreeRest,tPause=tPause,pBias=pBias,taxisMode=taxisMode,tFree=tFree,vFree=vFree,pos=pos[length(pos[,1]),2:4])
       if(catch[2]==1){
-        pos <- rbind(pos,catch[3:7])
+        pos <- rbind(pos,catch[3:6])
         i <- i+1
-        tfreerest <- catch[1]
+        tFreeRest <- catch[1]
       }
       else{
-        deltatrest <- catch[1]
+        deltaTrest <- catch[1]
         d <- catch[3:5]
-        tfreerest<-catch[6]
+        tFreeRest<-catch[6]
         paused = FALSE
       }
-      if( deltatrest > 0 ){
-        catch <- movingStep(deltatrest)
+      if( deltaTrest > 0 ){
+        catch <- .movingStep(i=i,deltaT=deltaTrest,pos=pos[length(pos[,1]),2:4],tFreeRest=tFreeRest,d=d)
         if(catch[2]==1){
-          tfreerest <- catch[1]
-          pos <- rbind(pos,catch[3:7])
+          tFreeRest <- catch[1]
+          pos <- rbind(pos,catch[3:6])
           i <- i+1
         }
         else{
-          deltatrest <- catch[1]
-          pos <- rbind(pos,catch[3:7])
+          tFreeRest <- 0.0
+          deltaTrest <- catch[1]
+          pos <- rbind(pos,catch[3:6])
           i <- i+1
         }
       }
     }
     else{
-      catch <- movingStep(deltat)
+      catch <- .movingStep(i=i,deltaT=deltaT,pos=pos[length(pos[,1]),2:4],tFreeRest=tFreeRest,d=d)
       if(catch[2]==1){
-        tfreerest <- catch[1]
-        pos <- rbind(pos,catch[3:7])
+        tFreeRest <- catch[1]
+        pos <- rbind(pos,catch[3:6])
         i <- i+1
       }
       else{
-        deltatrest <- catch[1]
-        pos <- rbind(pos,catch[3:7])
+        tFreeRest <- 0.0
+        deltaTrest <- catch[1]
+        pos <- rbind(pos,catch[3:6])
         i <- i+1
         paused <- TRUE
       }
-      if( deltatrest > 0 ){
-        catch <- pausedStep(deltatrest)
+      if( deltaTrest > 0 ){
+        catch <- .pausedStep(d=d,pPersist=pPersist,i=i,deltaT=deltaTrest,tFreeRest=tFreeRest,tPause=tPause,pBias=pBias,taxisMode=taxisMode,tFree=tFree,vFree=vFree,pos=pos[length(pos[,1]),2:4])
         if(catch[2]==1){
-          tfreerest <- catch[1]
-          pos <- rbind(pos,catch[3:7])
+          tFreeRest <- catch[1]
+          pos <- rbind(pos,catch[3:6])
           i <- i+1
         }
         else{
-          deltatrest <- catch[1]
+          deltaTrest <- catch[1]
         }
       }
     }
-    j <- j+1
-    #or should we use i here instead? it's more intuitive and predictable if we use i... 
-    if(j == nsteps){
+    if(i == nsteps+1){
       break
     }
   }
-  colnames(pos) <- c("Delta_t","TimePoint","PosX","PosY","PosZ")
+  colnames(pos) <- c("t","x","y","z")
   return(pos)
 }
