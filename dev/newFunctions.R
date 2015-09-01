c210c4839b37baac8da7ed813168a9df0bdcedb6
jberry47 <jberry47@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-01 16:45:43 +0000

    In beauWalker: -Found that the normalization of the bias.dir was bugged, it is now fixed -Instead of n.steps being the break element, a sim.time is used to determine if the sim should end.
    added correctDrift function that removes a constant drift from a single track
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motilitylab@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- dev/newFunctions.R
+++ dev/newFunctions.R
@@ -100,15 +100,17 @@
 
 #' Simulates a single cell track based on updated Beauchemin model written by J. Textor, M. Sinn, and R. Boer. 
 #' 
-#' @param n.steps Specifies the number of steps for the model to take.
-#' @param delta.t Change in time between each timepoint.
+#' @param sim.time Specifies the duration time of the simulation (in minutes). When at time point i, if (i+1)*delta.t > sim.time
+#' then the simulation ends. In otherwords, the actual simulation can only be either as long or just short of the allowed
+#' simulation time. 
+#' @param delta.t Change in time between each timepoint (in minutes).
 #' @param p.persist Indicates how probable a change in direction is. p.persist = 1 indicates there is never a 
 #' change in direction between steps and p.persist = 0 indicates there is always a change in direction between steps.
 #' @param p.bias Strength of movement in the direction of \code{bias.dir}.
 #' @param taxis.mode Specified mode of movement. 1 := orthotaxis, 2 := topotaxis, 3 := klinotaxis.
-#' @param t.free Time interval for how long the cell is allowed to move between steps.
+#' @param t.free Time interval for how long the cell is allowed to move between steps (in minutes).
 #' @param v.free Speed of cell.
-#' @param t.pause Time for how long the cell takes to adjust movement to new direction.
+#' @param t.pause Time for how long the cell takes to adjust movement to new direction (in minutes).
 #' @param bias.dir The three dimensional vector c(x,y,z) indicating the direction for which there is a preference for movement.
 #' @details The default parameters are those found by mean-square displacement plots created by Beauchemin et al.
 #' @return A matrix of size \code{n.steps}x5. Each row represents the three dimensional coordinates of the cell
@@ -128,7 +130,7 @@
 #" lapply(blist, function(i) points(y~x,data=i,type='b'))
 
 
-beauWalker <- function(n.steps = 10,delta.t=1,p.persist=0.5,p.bias=0.9,bias.dir=c(0,0,0),taxis.mode=1,t.free=2,v.free=18.8,t.pause=0.5){  
+beauWalker <- function(sim.time=10,delta.t=1,p.persist=0.5,p.bias=0.9,bias.dir=c(0,0,0),taxis.mode=1,t.free=2,v.free=18.8,t.pause=0.5){  
   #' Parameter checks
   if(p.persist < 0 || p.persist > 1){
     stop("p.persist must be a value between 0 and 1")
@@ -154,9 +156,12 @@ beauWalker <- function(n.steps = 10,delta.t=1,p.persist=0.5,p.bias=0.9,bias.dir=
   if(v.free <= 0){
     stop("v.free must be a value larger than 0")
   }
-  bias.dir <- bias.dir/sqrt(sum(bias.dir^2))
+
   
   #' Initializing parameters 
+  if(!all.equal(bias.dir,c(0,0,0))){
+    bias.dir <- bias.dir/sqrt(sum(bias.dir^2))
+  }
   pos <- matrix(c(0,0,0,0),1,4)
   t.free.rest <- t.free
   paused <- t.free.rest < 0.0
@@ -170,7 +175,7 @@ beauWalker <- function(n.steps = 10,delta.t=1,p.persist=0.5,p.bias=0.9,bias.dir=
     d <- d[1:3]
   }
   i <- 1
-  
+
   #' This is the primary loop that creates the 3d track
   repeat{
     if(paused==TRUE){
@@ -227,7 +232,8 @@ beauWalker <- function(n.steps = 10,delta.t=1,p.persist=0.5,p.bias=0.9,bias.dir=
         }
       }
     }
-    if(i >= n.steps+1){
+    if((i+1)*delta.t > sim.time){
+      print(c(i*delta.t,sim.time))
       break
     }
   }
@@ -251,4 +257,39 @@ fractalDimension <- function(track){
   fd.full <- fractaldim::fd.estim.boxcount(track[,2:ncol(track)])
   fd.out <- fd.full$fd
   return(fd.out)
+}
+
+
+
+
+#' Removes constant drift from all tracks within a \code{tracks} object
+#' 
+#' @param x The \code{tracks} object for which the drift is to be removed from
+#' @param drift A vector designating the direction of the drift
+#' @details This function is to be used only if there is evidence that for each time point the movement is characterized 
+#' by the equation:
+#'    track[i+1,] = track[i,]+trueMovement+drift
+#' Here drift is a constant vector that applies for all i thus the drift correction is simply subtracting out the designated 
+#' drift vector from the positions of all time points excluding the initial time point.
+#' @return A \code{tracks} object that has been corrected for drift
+#' @examples 
+#' blist <- list()
+#' for(i in 1:20){
+#'  blist[[i]]<-beauWalker(bias.dir=c(-1,1,0),p.bias=10,taxis.mode = 1,p.persist = 0.1,n.steps = 10,delta.t = 1)
+#' }
+#' blist.corrected <- correctDrift(blist,c(-1.5,.8,0))
+correctDrift<- function(x, drift=NULL){
+  if(is.null(drift)){
+    stop("Please specify a vector indicating direction of the drift")
+  }
+  if(class(x) != "tracks"){
+    stop("The supplied tracks argument is not a tracks object")
+  }
+  if(length(x)==0){
+    stop("There are no tracks in the supplied input tracks object")
+  }
+  if(ncol(x[[1]])-1 != length(drift)){
+    stop("The dimensions of drift do not match the dimensions of tracks")
+  }
+  return(as.tracks(lapply(x, function(i) rbind(i[1,],cbind(i[-1,1],i[2:nrow(i),2:ncol(i)]-drift)))))
 }
