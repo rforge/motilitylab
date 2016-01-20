d0fb26f99dee05c40f12d6991b9f6acbedd8edc5
johannes_textor <johannes_textor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-20 14:14:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/motilitylab@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EpitopePrediction/R/peptide-mhc.R
+++ pkg/EpitopePrediction/R/peptide-mhc.R
@@ -37,7 +37,7 @@ supportedMHCs <- function( l=NULL ){
 #' Get Prediction Matrix
 #'
 #' Loads the prediction matrix for a given MHC molecule at the given peptide length.
-#' Matrices are loaded from the package folder upon first load and are then cached in 
+#' Matrices are loaded from the package folder upon first load and are then cached in
 #' memory for further uses.
 #' The SMM method predicts binding by summing the matrix entries for each amino acid
 #' per position and then adding and MHC-dependant number. The resulting number is the
@@ -55,7 +55,7 @@ supportedMHCs <- function( l=NULL ){
 #' M["L",]
 #' ## Which amino acid is most preferred at the C-terminal position?
 #' names( which.min( M[,9] ) )
-#' 
+#'
 #' ## Naming of the MHC molecule is permissive
 #' for( mhc in c("HLA-A0201","HLA-A-0201","HLA-A*0201","HLA-A*02:01") ){
 #'    names( which.min( smmMatrix( "HLA-A-02:01", 9 )$M[,9] ) )
@@ -66,12 +66,12 @@ smmMatrix <- function( mhc="HLA-A-02:01", l=9 ){
 	mhc <- gsub( "HLA-([AB])-([0-9])", "HLA-\\1\\2", as.character(mhc) )
 	matrixname <- paste(gsub("[:*]","",mhc),"-",l,sep="")
 	if( exists( matrixname, .smm.cache ) ){
-		return( 
+		return(
 			list( M=get( matrixname, envir=.smm.cache ),
 				c=get( paste(matrixname,"-c",sep=""), envir=.smm.cache ) )
 		)
 	}
-	matrixfile <- system.file( "extdata", paste(matrixname,".txt",sep=""), 
+	matrixfile <- system.file( "extdata", paste(matrixname,".txt",sep=""),
 		package=getPackageName() )
 	if( matrixfile == "" ){
 		stop( paste( "SMM matrix for MHC",mhc,"and peptide length",l,"not found!") )
@@ -101,16 +101,16 @@ smmMatrix <- function( mhc="HLA-A-02:01", l=9 ){
 #' for all peptides of length \code{l}.
 #' @param quantile.threshold a number between 0 and 1. If this is not \code{NULL}, then
 #' the parameter \code{ic50.threshold} is ignored and the peptides for which the predicted
-#' IC50 falls within the given quantile will be returned. For instance, a value of .02 
+#' IC50 falls within the given quantile will be returned. For instance, a value of .02
 #' return the peptides whose binding strength is in the top 2\% for the given protein.
 #' @param include.peptide logical, whether to include the actual peptide in the output
-#' data frame. This may not be desired in some circumstances, e.g. for very long 
+#' data frame. This may not be desired in some circumstances, e.g. for very long
 #' proteins or for converting the result to a matrix.
 #' @param method string defining which prediction method to use. Currently the only
-#' implemented method is the stabilized matrix method (SMM), such that this setting is 
+#' implemented method is the stabilized matrix method (SMM), such that this setting is
 #' ignored. But further methods may be implemented in the future.
 #' @return a data frame containing the peptide (if \code{include.peptide=TRUE}),
-#' start position, end position, and predicted IC50 for 
+#' start position, end position, and predicted IC50 for
 #' every peptide below the threshold.
 #'
 #' @examples
@@ -121,7 +121,7 @@ smmMatrix <- function( mhc="HLA-A-02:01", l=9 ){
 #'  "PLGGAARALAHGVRVLEDGVNYATGNLPGCSFSIFLLALLSCLTVPASA",sep="")
 #' binders( hcv.core )
 #' @export
-binders <- function( x, mhc="HLA-A-02:01", l=9, ic50.threshold=500, 
+binders <- function( x, mhc="HLA-A-02:01", l=9, ic50.threshold=500,
 	quantile.threshold=NULL,
 	include.peptide=TRUE, method="smm" ){
 	x <- as.character(x)
@@ -146,8 +146,8 @@ binders <- function( x, mhc="HLA-A-02:01", l=9, ic50.threshold=500,
 
 #' Peptide-MHC Binding Prediction
 #'
-#' Predicts peptide-MHC binding using the stabilized matrix method (SMM) 
-#' algorithm with a specifically constructed amino acid substitution matrix. 
+#' Predicts peptide-MHC binding using the stabilized matrix method (SMM)
+#' algorithm with a specifically constructed amino acid substitution matrix.
 #' See \code{citation(package='EpitopePrediction')} for the reference.
 #'
 #' @param x vector of strings containing the peptides for which to predict MHC
@@ -155,13 +155,13 @@ binders <- function( x, mhc="HLA-A-02:01", l=9, ic50.threshold=500,
 #' @param mhc string or vector of strings identifying the MHC molecules. See
 #' \code{supportedMHCs} for allowed values. If a vector is given, it must be of the same
 #' length as \code{x}.
-#' @param output.IC50 whether to output the IC50 value itself (default) or its 
+#' @param output.IC50 whether to output the IC50 value itself (default) or its
 #' base10-logarithm.
 #' @examples
 #' ## Predict IC50 binding values for two famous peptides
 #' smm( c("SLYNTVATL","SYFPEITHI"), "HLA-A-02:01" )
 #' @export
-smm <- function( x=c("SLYNTVATL","SYFPEITHI"), mhc="HLA-A-02:01", 
+smm <- function( x=c("SLYNTVATL","SYFPEITHI"), mhc="HLA-A-02:01",
 	output.IC50=TRUE ){
 	if( length(mhc)>1 && ( length(mhc) != length(x) ) ){
 		stop( "If 'mhc' is a vector, it must have the same length as 'peptides'!" )
@@ -185,7 +185,7 @@ smm <- function( x=c("SLYNTVATL","SYFPEITHI"), mhc="HLA-A-02:01",
 #' Determine Anchor Positions
 #'
 #' Uses the information in the SMM matrices to predict anchor positions for a given MHC
-#' molecule. Specifically, SMM matrix columns are sorted by their variance, and the 
+#' molecule. Specifically, SMM matrix columns are sorted by their variance, and the
 #' \code{k} columns with lowest variance are defined as anchor positions.
 #'
 #' @param mhc the mhc molecules.
@@ -205,13 +205,13 @@ anchorPositions <- function( mhc="HLA-A-02:01", l=9, k=2 ){
 #' Plot an MHC Binding Motif
 #'
 #' Visualizes the set of peptides which are predicted to bind to a certain MHC molecule.
-#' 
+#'
 #' @param mhc name of the MHC molecule.
 #' @param l length of the peptide.
-#' @param motif.matrix a matrix. If this is not \code{NULL}, then the parameters 
+#' @param motif.matrix a matrix. If this is not \code{NULL}, then the parameters
 #' \code{mhc} and \code{l} are ignored and the supplied matrix is used instead. This
-#' makes it possible to use this function for plotting sequence logos unrelated to 
-#' MHC-peptide binding. The supplied matrix must have row names to indicate the 
+#' makes it possible to use this function for plotting sequence logos unrelated to
+#' MHC-peptide binding. The supplied matrix must have row names to indicate the
 #' letters out of which the sequence logo is made.
 #' @param width bar width for profile per position.
 #' @param space amount of space left before each bar, as a fraction of the
@@ -229,7 +229,7 @@ anchorPositions <- function( mhc="HLA-A-02:01", l=9, k=2 ){
 #' @export
 plotBindingMotif <- function( mhc="HLA-A-02:01",
 		l=9, motif.matrix=NULL, width=.5,
-		space=1, col=rainbow(20), main=paste(mhc,", ",l,"-mers",sep=""), 
+		space=1, col=rainbow(20), main=paste(mhc,", ",l,"-mers",sep=""),
 		... ){
 	if( is.null( motif.matrix ) ){
 		M <- 10^(-smmMatrix( mhc, l )$M)
@@ -238,9 +238,13 @@ plotBindingMotif <- function( mhc="HLA-A-02:01",
 		l <- ncol(M)
 	}
 	M <- scale(M,center=FALSE,scale=colSums(M))
-	colEntropies <- apply( M, 2, function(x) log(20)+sum(x*log(x)) )
+	colEntropies <- apply(M, 2, function(x){
+		x <- x*log(x)
+		x[is.nan(x)] <- 0
+		log(20) + sum(x)
+  })
 	M <- scale(M,center=FALSE,scale=1/colEntropies)
-	barplot(M,col=col,width=width,space=space,main=main,...)		
+	barplot(M,col=col,width=width,space=space,main=main,...)
 	for( i in seq_len( l ) ){
 		ypos <- cumsum(M[,i])-M[,i]/2
 		showl <- M[,i]>1.5*strheight("M")
@@ -250,4 +254,3 @@ plotBindingMotif <- function( mhc="HLA-A-02:01",
 	}
 	axis(1,at=(1:l)*(width*(1+space))-width/2,labels=1:l)
 }
-
